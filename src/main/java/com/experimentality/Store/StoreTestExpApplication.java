9e1d65c31bc16cecc96bb9ad4fd861d385109555
Juan Arango <jblackheart581@gmail.com>
2021-05-10 23:18:15 -0500

    First commit to master

---
+++ src/main/java/com/experimentality/Store/StoreTestExpApplication.java
@@ -0,0 +1,13 @@
+package com.experimentality.Store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoreTestExpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StoreTestExpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/experimentality/Store/StoreTestExpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.experimentality.Store;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreTestExpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
